2dc7da117d0618c45ab3aa6895752b17a54fbc34
Guillaume <gparis@et.esiea.fr>
2023-10-14 12:03:19 +0200

    reading test from standard input

--- src/main.c
+++ src/main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-void main(void)
-{
-    printf("Hello World!\n");
+#define MAX_BUFFER_SIZE 255
+int main(int argc, char *argv[]){
+	//test 1: lire depuis l'entrée standard stdin et écrire dans un fichier
+	
+	//on ouvre le fichier de sortie
+	FILE *output_file = fopen(argv[1], "w");
+    if (output_file == NULL) {
+        perror("Error opening output file");
+        exit(EXIT_FAILURE);
+    }
+    
+    char buffer[MAX_BUFFER_SIZE];
+    while (fgets(buffer, MAX_BUFFER_SIZE, stdin) != NULL) {
+    	fprintf(output_file, buffer);
+    }
+    
+    fclose(output_file);
+
+    return 0;
+		
 }
