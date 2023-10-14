cca033e5f851650aa8a3372e9fb963efa3b613a8
Guillaume <gparis@et.esiea.fr>
2023-10-14 16:28:47 +0200

    add matching test

--- src/main.c
+++ src/main.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/md5.h>
+#include <stdbool.h>
+#include <openssl/md5.h> //nécessite libssl-dev
+
+//cat rockyou.txt | ./exec -G rainbow_table.txt
+//cat hashes_input.txt | ./exec -L rainbow_table.txt
 
 #define MAX_BUFFER_SIZE 1024
 #define MD5_DIGEST_LENGTH 16
 
+/*******************************/
+//Hashage
 void generation_hash_MD5(unsigned char hash[MD5_DIGEST_LENGTH], char *string){
 	MD5_CTX ctx;
 	MD5_Init(&ctx);
@@ -31,6 +37,7 @@ void hash_to_file(FILE *file, unsigned char mdp_hash[MD5_DIGEST_LENGTH], char * 
 }
 
 /********************************/
+//Structure
 typedef struct T3c {
 	char *mdp_clair;
 	unsigned char hash[MD5_DIGEST_LENGTH];
@@ -76,6 +83,28 @@ void freeList(t3c *tete) {
 }
 
 /********************************/
+//Comparaison
+//args: 2 hashs. si correspondance, retourne vrai
+
+char *hexEncode(const unsigned char *hash, size_t length) {
+    char *hexString = malloc(length * 2 + 1);
+
+    for (size_t i = 0; i < length; i++) {
+        sprintf(hexString + i * 2, "%02x", hash[i]);
+    }
+
+    return hexString;
+}
+
+bool comparaison(char *input_hash, unsigned char *rainbow_hash){
+	bool cracked = false;
+	char *hexString = hexEncode(rainbow_hash, MD5_DIGEST_LENGTH);
+	if(strcmp(input_hash, hexString)==0) cracked = true;
+	free(hexString);
+	return cracked;
+}
+
+/********************************/
 
 int main(int argc, char *argv[]){
 	//printf("mode: %s\n", argv[1]);
@@ -111,13 +140,37 @@ int main(int argc, char *argv[]){
 		fclose(output_file);
 	}
 	else if(strcmp(argv[1], "-L")==0){ //Mode de comparaison stdin(hashs) | ./main.c mode rainbow ou ./main.c mode rainbow hashs
-		//test 1: charger la rainbow table dans la structure t3c
+		//test 2: fonction de comparaison entre stdin et struct t3c
 		//on ouvre la rainbow table
 		FILE *rainbow = fopen(argv[2], "r");
 		if (rainbow == NULL) {
 			perror("Error opening output file");
 			exit(EXIT_FAILURE);
 		}
+
+	/*char buffer2[MAX_BUFFER_SIZE];
+		while (fgets(buffer2, MAX_BUFFER_SIZE, stdin) != NULL) {
+			size_t length = strlen(buffer2);
+			if (length > 0 && buffer2[length - 1] == '\n') { //remplace \n par \0 dans le buffer
+				buffer2[length - 1] = '\0';
+			}
+			printf("%s\n", buffer2);
+		}*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 		t3c *liste = create();
 
@@ -140,17 +193,44 @@ int main(int argc, char *argv[]){
 
 				count++;
 				if (count % 1000000 == 0) {
-				  printf("INFO Loading %d hashes...\n", count);
-				  }
+					printf("INFO Loading %d hashes...\n", count);
+				}
 
 			}
 		}
 		printf("INFO %d hashes loaded.\n", count);
-		
+
+
+
+		fclose(rainbow);
+		//Fin du chargement en mémoire
+
+		//Recherche de correspondance entre les input hashs et les rainbow hashs
+		char buffer2[MAX_BUFFER_SIZE];
+		while (fgets(buffer2, MAX_BUFFER_SIZE, stdin) != NULL) {
+			size_t length = strlen(buffer2);
+			if (length > 0 && buffer2[length - 1] == '\n') { //remplace \n par \0 dans le buffer
+				buffer2[length - 1] = '\0';
+			}
+			//printf("%s\n", buffer2);
+
+			t3c *tmp = liste;
+			while (tmp != NULL) {
+				if(comparaison(buffer2, tmp->hash)){
+					printf("MATCH ");
+					
+					for (int i = 0; i < MD5_DIGEST_LENGTH; i++) {
+						printf("%02x", tmp->hash[i]);
+					}
+					printf(" %s\n", tmp->mdp_clair);
+					break;
+				}
+				tmp = tmp->next;
+			}
+		}
+
 		// Libération de la mémoire allouée par la liste
 		freeList(liste);
-
-		fclose(rainbow);	
 
 	}
 	else {
